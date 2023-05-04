044a7feb788e7c119f2858c7c5c2125a0ca9a226
Lucas Raoni Hideki Antunes <85591742+mahauni@users.noreply.github.com>
2023-05-04 11:08:44 -0300

    Update UserDAO.java

--- src/Database/DAO/UserDAO.java
+++ src/Database/DAO/UserDAO.java
@@ -2,7 +2,10 @@ package Database.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import Database.Connection.ConnectionFactory;
 import Database.Models.User;
@@ -17,7 +20,7 @@ public class UserDAO {
 	
 	//insert
 	public void insert(User user) throws SQLException {
-		String sql = "insert into USUARIO(id,nome,email,senha,dataregistro) VALUES(?,?,?,?,?)";
+		String sql = "INSERT INTO USUARIOS(ID, NAME, EMAIL, SENHA, DATAREGISTRO) VALUES(?, ?, ?, ?, ?)";
 		
 		PreparedStatement stmt = connection.prepareStatement(sql);
 		stmt.setLong(1, user.getId());
@@ -31,7 +34,59 @@ public class UserDAO {
 	}
 	
 	//delete
+	public void delete(Long id) {
+		String sql = "DELETE FROM USUARIOS WHERE ID=?";
+		try {
+		    PreparedStatement stmt = connection.prepareStatement(sql);
+		    stmt.setLong(1, id);
+
+		    stmt.execute();
+		    stmt.close();
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		}
+    	}
+	
 	//update
+	public void update(User user) {
+		String sql = "UPDATE USUARIOS SET NAME=?, EMAIL=?, SENHA=? WHERE ID=?";
+
+		try {	
+		    PreparedStatement stmt = connection.prepareStatement(sql);
+
+		    stmt.setString(1, user.getName());
+		    stmt.setString(2, user.getEmail());
+		    stmt.setString(3, user.getPassword());
+		    stmt.setLong(4, user.getId());
+
+		    stmt.execute();
+		    stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	//selectALL
+	public List<User> select() throws SQLException {
+		List<User> users = new ArrayList<User>();
+		String sql = "SELECT * FROM USUARIOS";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+
+		while (rs.next()) {
+			User user = new User();
+			user.setId(rs.getLong("ID"));
+			user.setName(rs.getString("NAME"));
+			user.setEmail(rs.getString("EMAIL"));
+			user.setPassword(rs.getString("SENHA"));
+			user.setRegisterDate(rs.getDate("DATAREGISTRO"));
+
+			users.add(user);
+		}
+		rs.close();
+		stmt.close();
+		return users;
+	}
+	
 	//selectById
 }

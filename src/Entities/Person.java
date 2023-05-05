f1c6534b58ee74756a35df42e4c22e33655a7754
Lucas Raoni <lucasraoniha@gmail.com>
2023-05-05 13:00:38 -0300

    Added entities and created person entities

---
+++ src/Entities/Person.java
@@ -0,0 +1,12 @@
+package Entities;
+
+import JavaBeans.Problem;
+import JavaBeans.URL;
+
+import java.util.ArrayList;
+
+public class Person {
+    private JavaBeans.Person person;
+    private ArrayList<URL> urls;
+    private ArrayList<Problem> problems;
+}
